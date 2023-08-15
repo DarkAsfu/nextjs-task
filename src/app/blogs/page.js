b7428d05b9cce0e9587e003060df8d1c91b1cc7b
DarkAsfu <ashraful.islam.asfu@gmail.com>
2023-08-15 13:10:05 +0600

    Blog page show all card

--- src/app/blogs/page.js
+++ src/app/blogs/page.js
@@ -1,15 +1,14 @@
 import BlogCard from '@/components/BlogCard';
 import loadAllBlogData from '@/utils/loadAllBlogData';
-import Image from 'next/image';
 import React from 'react';
 
 const BlogsPage = async () => {
     const blogs = await loadAllBlogData();
-    console.log(blogs);
+    // console.log(blogs);
     return (
         <div className='w-11/12 mx-auto my-10'>
-            <h1>Blogs : {blogs.length}</h1>
-            <div>
+            <h1 className='text-xl font-bold my-3'>Number of Blogs : {blogs.length}</h1>
+            <div className='grid md:grid-cols-3 gap-10'>
                 {
                     blogs.map(blog => <BlogCard key={blog.id} blog={blog}></BlogCard>)
                 }
--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -1,21 +1,34 @@
 import Image from 'next/image';
 import React from 'react';
 
-const BlogCard = () => {
+const BlogCard = ({ blog }) => {
+    const { id, title, author, date, image, content } = blog;
     return (
-        <div className="card w-96 bg-base-100 shadow-xl">
-            <figure><Image width={300} height={300} src="https://i.ibb.co/qsdkRbH/pexels-pixabay-261579.jpg" alt="Shoes" /></figure>
-            <div className="card-body">
-                <h2 className="card-title">
-                    Shoes!
-                    <div className="badge badge-secondary">NEW</div>
-                </h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
-                <div className="card-actions justify-end">
-                    <div className="badge badge-outline">Fashion</div>
-                    <div className="badge badge-outline">Products</div>
-                </div>
+        <div className="rounded overflow-hidden shadow-lg">
+            <div href="#"><div className="relative">
+                <Image width={400} height={300} className="w-full" src="https://i.ibb.co/qsdkRbH/pexels-pixabay-261579.jpg" alt="Sunset in the mountains"/>
+                    <div className="hover:bg-transparent transition duration-300 absolute bottom-0 top-0 right-0 left-0 bg-gray-900 opacity-25"></div>
+                    <a href="#!"><span className="absolute bottom-0 left-0 bg-indigo-600 px-4 py-2 text-white text-sm hover:bg-white hover:text-indigo-600 transition duration-500 ease-in-out">
+                        Photos
+                    </span></a>
+                    <div href="!#"><div className="text-sm absolute top-0 right-0 bg-indigo-600 px-4 text-white rounded-full h-16 w-16 flex flex-col items-center justify-center mt-3 mr-3 hover:bg-white hover:text-indigo-600 transition duration-500 ease-in-out">
+                        <span className="font-bold">New</span>
+                        <small>Blogs</small>
+                    </div></div>
+            </div></div>
+            <div className="px-6 py-4">
+                <a href="#" className="font-semibold text-lg inline-block hover:text-indigo-600 transition duration-500 ease-in-out">{title}</a>
+                <p className="text-gray-500 text-sm">
+                    {content.slice(0,50)}...see more
+                </p>
             </div>
+            <div className="px-6 py-4 flex justify-between items-center">
+                <span className="py-1 text-sm font-regular text-gray-900 mr-1 flex flex-row items-center">
+                    <span className="ml-1">{author}</span>
+                </span>
+                <span className="ml-1">{date}</span>
+            </div>
+            <button className='px-6 bg-blue-600 text-white mb-10 ml-6 py-3 rounded-sm'>Details</button>
         </div>
     );
 };
