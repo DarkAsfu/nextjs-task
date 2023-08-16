6206135bb64315072a04fd7cd0c855e86652e41d
DarkAsfu <ashraful.islam.asfu@gmail.com>
2023-08-16 17:44:13 +0600

    deploy

--- src/app/blogs/[id]/page.js
+++ src/app/blogs/[id]/page.js
@@ -9,7 +9,7 @@ export const metadata = {
 const SingleBlog = async ({ params }) => {
     const id = params.id;
     console.log(id);
-    const res = await fetch(`https://nextjs-task-ochre.vercel.app/api/blogs/${id}`)
+    const res = await fetch(`https://nextjs-task-git-main-darkasfu.vercel.app/api/blogs${id}`)
     const blog = await res.json();
     console.log(blog);
     const { title, content, image, date, author } = blog;
