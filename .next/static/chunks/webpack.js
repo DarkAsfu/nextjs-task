40a8457866b2cae6957fa807dbccb7133a45b976
DarkAsfu <ashraful.islam.asfu@gmail.com>
2023-08-16 16:20:08 +0600

    Change image link

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -180,7 +180,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "eae68d3e8131a807"; }
+/******/ 		__webpack_require__.h = function() { return "b8a81f6e5238386c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
